28eb02c3264ba8be2ad72aa609e8781aae6f3654
myzra <myzrahfortnite@gmail.com>
2025-08-25 02:00:59 +0200

    maven project init

---
+++ soundboardfx/src/main/java/com/maven/soundboard/soundboardfx/App.java
@@ -0,0 +1,13 @@
+package com.maven.soundboard.soundboardfx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Test" );
+    }
+}
